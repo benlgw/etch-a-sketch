dfe87e4784d63fb9693276f6e8506f048a7042da
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-01 17:40:07 +0100

    added event listener for adding color

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const etchSketch = document.querySelector("#etchSketch");
 
-for (let r = 1; r <= 16; r++) {
+let size = 16;
+
+for (let r = 1; r <= size; r++) {
 	const row = document.createElement("div");
 	row.classList.add("row");
 	// row.textContent = r;
-	for (let c = 1; c <= 16; c++) {
+	for (let c = 1; c <= size; c++) {
 		const column = document.createElement("div");
 		column.classList.add("column");
-		column.textContent = c;
+		column.setAttribute("id", "column");
+		// column.textContent = c;
 		row.append(column);
 	}
 	etchSketch.append(row);
 }
+
+const pixels = document.querySelectorAll("#column");
+const test = document.querySelector("p");
+
+pixels.forEach((element) => {
+	element.addEventListener("mouseover", (event) => {
+		element.setAttribute("style", "background-color: red");
+	});
+});
